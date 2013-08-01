8265345babe722e49f553d54c798b406f16e3e35
Marco Pessotto <melmothx@gmail.com>
2013-08-01 16:51:28 +0200

    Implemented the request for the quantum view sheet.

--- lib/Shipping/UPS/Tiny/QuantumView.pm
+++ lib/Shipping/UPS/Tiny/QuantumView.pm
@@ -3,7 +3,16 @@ package Shipping::UPS::Tiny::QuantumView;
 use 5.010000;
 use strict;
 use warnings FATAL => 'all';
+use Date::Parse;
 use Moo;
+use File::Spec;
+use XML::Compile::Schema;
+use XML::LibXML;
+use XML::LibXML::Simple;
+use LWP::UserAgent;
+use HTTP::Request;
+use POSIX qw/strftime/;
+
 
 =head1 NAME
 
@@ -17,13 +26,36 @@ Shipping::UPS::Tiny::QuantumView -- class for QuantumView information
 
 =item account_key
 
-The kye provided by UPS
+The key provided by UPS
 
 =item username
 
 The username
 
 =item password
+
+The password
+
+=item endpoint
+
+By default: https://wwwcie.ups.com/ups.app/xml/QVEvents (testing)
+
+In production, you have to set this to
+https://onlinetools.ups.com/ups.app/xml/QVEvents
+
+This is a read-only accessor, so you have to set it in the
+constructor (or calling the Tiny module with
+
+  $ups->quantum_view(endpoint => 'https://onlinetools.ups.com/ups.app/xml/QVEvents');
+
+=item schemadir
+
+The directory in which the XML schema files can be found. Mandatory.
+
+=item subscription_name
+
+The named subscription. This is a read-write accessor, so you can set
+it after the object has been built.
 
 =back
 
@@ -37,6 +69,222 @@ has 'username' => (is => 'ro',
 
 has 'password' => (is => 'ro',
                    required => 1);
+
+has endpoint => (is => 'ro',
+                 default => sub { return 'https://wwwcie.ups.com/ups.app/xml/QVEvents' });
+
+has schemadir => (is => 'ro',
+                  isa => sub {
+                      die "$_[0] is not a dir" unless -d $_[0];
+                  },
+                  required => 1);
+
+has subscription_name => (is => 'rw');
+
+=head1 METHODS
+
+=over 4
+
+=item fetch(begin => "18/dec/2013", end => "22/dec/2012")
+
+The main method. Fetch the data from UPS and return them as an hashref.
+Options (mutually exclusive):
+
+  unread => 1
+  days => 1
+  begin => "time string"
+
+Option when "begin" is specified:
+
+  end => "time string"
+
+The "time string" can be in any format supported by Date::Parse
+
+The value of C<days> specified the number of previous days for which
+we need the data.
+
+The value of C<unread> specified the number or request to make. Given
+that we can have a lot of traffic, unread could download a B<lot> of
+data. So with the value passed you can set the number of requests
+(each being aproximately 250kb).
+
+Default: days => 1 (one day)
+
+=back
+
+=cut
+
+sub fetch {
+    my $self = shift;
+    die "Unpaired options" if @_ % 2;
+    my %args = @_;
+    if ((keys %args) > 1) {
+        die "mutually exclusive options passed"
+          unless ($args{begin} && $args{end});
+    }
+
+    if ($args{unread}) {
+        return $self->_fetch_unread($args{unread});
+    }
+
+    if ($args{begin} && $args{end}) {
+        my $starttime = str2time($args{begin});
+        my $endtime   = str2time($args{end});
+        die "Wrong begin time format!" unless $starttime;
+        die "Wrong end time format!" unless $endtime;
+        return $self->_fetch_range($starttime, $endtime);
+    }
+
+    # fallback
+    if (my $days = $args{days} || 1) {
+        my $endtime = time();
+        my $starttime = $endtime - ($days * 60 * 60 * 24);
+        return $self->_fetch_range($starttime, $endtime);
+    }
+
+}
+
+sub _fetch_unread {
+    my $self = shift;
+    return $self->_retrieve;
+}
+
+# sub _fetch_unread {
+#     my ($self, $num) = @_;
+#     die "Wrong num $num!" if $num < 0;
+#     my $res = {};
+#     while ($num > 0) {
+#         $res = $self->_retrieve(bookmark => $res->{bookmark});
+#         last unless $res->{bookmark};
+#         $num--;
+#     }
+#     if ($res->{bookmark}) {
+#         warn "Not all the data has been fetched!";
+#     }
+# }
+# 
+
+sub _fetch_range {
+    my ($self, $beg, $end) = @_;
+    my $res = $self->_retrieve(beg => $beg,
+                               end => $end);
+}
+
+has debug_request => (is => 'rwp',
+                      default => sub { return "" });
+
+
+has ua => (is => 'ro',
+           default => sub {
+             return LWP::UserAgent->new();  
+           });
+
+
+sub _retrieve {
+    my ($self, %args) = @_;
+    my $xml = $self->access_request_xml;
+    $xml .= $self->_write_xml_from_schema("QuantumViewRequest",
+                                          $self->request_hash(%args));
+    my $req = HTTP::Request->new(POST => $self->endpoint);
+    $req->content($xml);
+    $self->_set_debug_request($xml);
+    return $self->ua->request($req);
+}
+
+=head2 The request
+
+The request is composed by two concatenated xml docs. The first part
+is the access request, which cannot change. The second is the request
+itself, which has a few options.
+
+=over 4 
+
+=item access_request_hash
+
+Hashref with key, user, password, to feed the XML compiler.
+
+=item access_request_xml
+
+Returns the XML I<string> and caches it for future invocations.
+
+=cut
+
+
+sub access_request_hash {
+    my $self = shift;
+    return {
+            AccessLicenseNumber => $self->account_key,
+            UserId => $self->username,
+            Password => $self->password,
+           };
+}
+
+has _cache_access_request_xml => (is => 'rw');
+
+sub _write_xml_from_schema {
+    my ($self, $schemaname, $hashref) = @_;
+    my $schemafile = File::Spec->catfile($self->schemadir,
+                                         "$schemaname.xsd");
+    die "Missing $schemafile" unless -f $schemafile;
+    my $schema = XML::Compile::Schema->new($schemafile);
+    my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
+    my $writer = $schema->compile(WRITER => $schemaname);
+    my $xml = $writer->($doc, $hashref);
+    $doc->setDocumentElement($xml);
+    return $doc->toString;
+}
+
+
+sub access_request_xml {
+    my $self = shift;
+    if (my $xml = $self->_cache_access_request_xml) {
+        return $xml;
+    }
+    # cache it
+    my $xmlstring = $self->_write_xml_from_schema("AccessRequest",
+                                                  $self->access_request_hash);
+    $self->_cache_access_request_xml($xmlstring);
+    return $xmlstring;
+}
+
+sub request_hash {
+    my ($self, %args) = @_;
+
+    # basically this could be enough to get all the unread data
+    my $qvr = { Request => {
+                            RequestAction => 'QVEvents'
+                           }
+              };
+
+    # but if we have a named subscription, use that
+    if (my $subscription_name = $self->subscription_name) {
+        $qvr->{SubscriptionRequest}->{Name} = $subscription_name;
+    }
+    
+    # and, if we have args with begin/end, we throw that in too
+    if ($args{beg} && $args{end}) {
+        $qvr->{SubscriptionRequest}->{DateTimeRange}->{BeginDateTime} =
+          _time_to_ups_format($args{beg});
+        $qvr->{SubscriptionRequest}->{DateTimeRange}->{EndDateTime} =
+          _time_to_ups_format($args{end});
+    }
+    elsif ($args{beg} || $args{end}) {
+        die "missing arguments to pass a range <$args{beg}> <$args{end}>!"
+    }
+
+    # and if we have a bookmark, pick that up
+    if ($args{bookmark}) {
+        $qvr->{Bookmark} = $args{bookmark};
+    }
+    return $qvr;
+}
+
+sub _time_to_ups_format {
+    my $time = shift;
+    return strftime('%Y%m%d%H%M%S', localtime($time));
+}
+
+
 
 
 1;
--- t/quantum_view.t
+++ t/quantum_view.t
@@ -7,8 +7,6 @@ use File::Spec::Functions;
 use Data::Dumper;
 use Test::More;
 
-plan tests => 4;
-
 my $conffile = catfile(t => 'conf.yml');
 
 unless (-f $conffile) {
@@ -16,18 +14,63 @@ unless (-f $conffile) {
     exit;
 }
 
+my $schema_dir = catdir(qw/t QuantumView QuantumViewforPackage
+                           QUANTUMVIEWXML Schemas/);
+
+unless (-d $schema_dir) {
+    plan skip_all => "Please get the QuantumView zip and unpack it into the 't' directory if you want to run the tests" ;
+    exit;
+}
+
+
+
+plan tests => 9;
+
+
+
 my $conf = LoadFile($conffile);
 
 my $ups = Shipping::UPS::Tiny->new(
                                    %{ $conf->{account} }
                                   );
 
-my $qv = $ups->quantum_view;
+my $qv = $ups->quantum_view(schemadir => $schema_dir);
 
 print Dumper($qv);
 ok($qv, "object initialized");
 ok($qv->username, "Username present");
 ok($qv->account_key, "Key present");
 ok($qv->password, "Password present");
+is($qv->endpoint, 'https://wwwcie.ups.com/ups.app/xml/QVEvents',
+   "endpoint is correct (wwwcie)");
+my $got = $qv->access_request_xml;
+diag $qv->access_request_xml;
+is ($qv->_cache_access_request_xml, $got, "Cache ok");
 
+$qv->subscription_name("ExceptionandDeliver");
+$got = $qv->request_hash(beg => 10, end => 100, bookmark => '1234');
 
+my $expected = {
+                'SubscriptionRequest' => {
+                                          'DateTimeRange' => {
+                                                              'BeginDateTime' => '19700101010010',
+                                                              'EndDateTime'   => '19700101010140'
+                                                             },
+                                          Name => "ExceptionandDeliver",
+                                         },
+                'Bookmark' => '1234',
+                'Request' => {
+                              'RequestAction' => 'QVEvents'
+                             }
+               };
+
+is_deeply($got, $expected, "Request hash is correct");
+
+ok ($qv->fetch(begin => '2013-07-01',
+               end => '2013-08-01')->is_success, "request Ok");
+
+ok($qv->fetch(days => 7)->is_success, "request OK");
+
+my $res = $qv->fetch(unread => 1);
+$res->is_success;
+print substr($res->content, 0, 1000);
