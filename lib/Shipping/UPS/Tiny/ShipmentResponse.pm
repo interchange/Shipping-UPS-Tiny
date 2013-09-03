2ff674d04834c066960bb3f0429dd7dabdc90a35
Marco Pessotto <melmothx@gmail.com>
2013-09-03 07:32:59 +0200

    Moved (verbatim) the base methods for success/failure to base class
    Removed shared methods from ShipmentResponse.pm

--- lib/Shipping/UPS/Tiny/ShipmentResponse.pm
+++ lib/Shipping/UPS/Tiny/ShipmentResponse.pm
@@ -4,6 +4,9 @@ use 5.010000;
 use strict;
 use warnings FATAL => 'all';
 use Moo;
+
+extends 'Shipping::UPS::Tiny::ResponseBase';
+
 use Data::Dumper;
 use File::Spec;
 use MIME::Base64 qw/decode_base64/;
@@ -15,113 +18,13 @@ Shipping::UPS::Tiny::ShipmentResponse -- response class for Shipment requests
 
 =head1 ACCESSORS/METHODS
 
+This class extends the base class
+L<Shipping::UPS::Tiny::ResponseBase>, which provides some share
+methods and accessors.
+
+Additionally, this class provides the following methods:
+
 =over 4
-
-=item raw_response
-
-The raw hashref with the parsed response.
-
-=item is_success
-
-Return true if the request reported a success.
-
-=item is_fault
-
-If true, the code and descriptions of the fault(s) are returned as a
-single string, meant to be displayed to the final user.
-
-=item alert
-
-Return the string with the alert(s), or the empty string.
-
-=cut
-
-has raw_response => (is => 'ro',
-                     required => 1,
-                     trigger => 1,
-                     isa => sub {
-                         die "the response must be an hashref"
-                           unless ref($_[0]) eq 'HASH';
-                     });
-
-
-has _response => (is => 'rw');
-has _fault => (is => 'rw');
-has _result => (is => 'rw');
-
-sub _trigger_raw_response {
-    # set the various hashrefs
-    my $self = shift;
-    my $raw = $self->raw_response;
-    # warn Dumper($raw);
-    # it's not clear when the root is "Body" or the keys sit on the
-    # the root.
-
-    if ((exists $raw->{Fault}) or
-        (exists $raw->{Body} and exists $raw->{Body}->{Fault}) or
-        (exists $raw->{ShippingError})) {
-        $self->_store_error_details;
-    }
-
-    if (exists $raw->{Body} and exists $raw->{Body}->{Response}) {
-        $self->_response($raw->{Body}->{Response});
-    }
-    elsif (exists $raw->{Response}) {
-        $self->_response($raw->{Response});
-    }
-
-    if (exists $raw->{Body} and exists $raw->{Body}->{ShipmentResults}) {
-        $self->_result($raw->{Body}->{ShipmentResults});
-    }
-    elsif (exists $raw->{ShipmentResults}) {
-        $self->_result($raw->{ShipmentResults});
-    }
-
-
-}
-
-
-
-sub is_fault {
-    my $self = shift;
-    my $desc = "";
-    if (my $faults = $self->_fault) {
-        foreach my $f (@$faults) {
-            $desc .= $f->{PrimaryErrorCode}->{Description} . " (" .
-              $f->{PrimaryErrorCode}->{Code} . ")\n";
-        }
-    }
-    return $desc;
-}
-
-sub alert {
-    my $self = shift;
-    my $out = "";
-    return "" unless $self->_response;
-    if (exists $self->_response->{Alert}) {
-        my $alert = $self->_response->{Alert};
-        if (ref($alert) eq 'ARRAY') {
-            foreach my $al (@$alert) {
-                $out .= $al->{Description} . "( " . $al->{Code} . ")\n";
-            }
-        }
-        elsif (ref($alert) eq 'HASH') {
-            $out = $alert->{Description} . "( " . $alert->{Code} . ")\n";
-        }
-    }
-    return $out;
-}
-
-
-sub is_success {
-    my $self = shift;
-    if ($self->_response) {
-        if ($self->_response->{ResponseStatus}->{Code} eq '1') {
-            return 1;
-        }
-    }
-    return 0;
-}
 
 =item ship_id
 
